2fa1e6de51ff07a42f4f4315d71abb3605a5c751
avalxnchx <avaransbotham@gmail.com>
2023-02-09 20:45:18 -0500

    rough draft

---
+++ download-data.R
@@ -0,0 +1,14 @@
+
+library(tidyverse)
+library(tidycensus)
+
+
+dallas_bachelors <- 
+  get_acs(geography = "tract",
+          variables = "DP02_0068P",
+          year = 2020,
+          state = "TX",
+          county = "Dallas",
+          geometry = TRUE)
+
+write_rds(dallas_bachelors, "data/dallas.rds")
